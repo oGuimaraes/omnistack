c793a333dd15b7b8772d6fe2f176ec0be33412cf
Otávio Guimarães <otaviovgsr@gmail.com>
2020-02-06 16:21:12 -0300

    mongodb connected via mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 
-app.get('/', (request, response) => {
+mongoose.connect('mongodb+srv://oguimaraes:otavio22@cluster0-cx1n7.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+app.use(express.json()); // Cadastrar para express entenda requisições no formato json
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criaçao ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+app.post('/users/:id', (request, response) => {
+    console.log(request.body);
     return response.json({ message: 'Hello Teste'});
 })
 
